8c5ecd351df639cb31e3292d24033a9275476379
Daniel Rodrigues - Grupo DPSP <daniel.crodrigues@dpsp.com.br>
2024-03-26 00:26:54 -0300

    feat: Reajusta codigo para fazer copias do repositorio original

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/rodrigues-daniel/gobranch
 
 go 1.21.3
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/ktrysmt/go-bitbucket v0.9.77 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,22 +2,100 @@ package main
 
 import (
 	"fmt"
-	"github.com/ktrysmt/go-bitbucket"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-	// Substitua 'seu-nome-de-usuario' e 'sua-senha' pelas credenciais reais
-	client := bitbucket.NewBasicAuth("seu-nome-de-usuario", "sua-senha")
 
-	// Faça uma solicitação para listar seus repositórios
-	repos, err := client.Repositories.ListForAccount()
+	origem := "dpsp-lmp-cornershop-carga-item"
+
+	// Cria o diretório
+	err := os.Mkdir("repositorios", 0755)
 	if err != nil {
-		fmt.Println("Erro ao listar repositórios:", err)
+		fmt.Println("Erro ao criar o diretório:", err)
 		return
 	}
 
-	// Imprima os nomes dos repositórios
-	for _, repo := range repos {
-		fmt.Println("Nome do Repositório:", repo.Name)
+	errcd := os.Chdir("repositorios")
+	if errcd != nil {
+		fmt.Println("Erro ao mudar de diretório:", errcd)
+		return
 	}
+
+	// Comando para criar um arquivo
+	cmd3 := exec.Command("git", "clone", "http://daniel.crodrigues@bitbucket:7990/scm/lmp/"+origem+".git")
+	if err := cmd3.Run(); err != nil {
+		fmt.Println("Erro ao tentar clonar:", err)
+		return
+	}
+
+	for i := 2; i <= 5; i++ {
+		destino := fmt.Sprintf("%s-%d", origem, i)
+		err := copiarPasta(origem, destino)
+		if err != nil {
+			fmt.Printf("Erro ao copiar pasta: %v\n", err)
+			break
+		}
+		fmt.Printf("Pasta copiada e renomeada para %s\n", destino)
+	}
+
+	// Obter o diretório atual
+	diretorioAtual, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Erro ao obter o diretório atual:", err)
+		return
+	}
+
+	fmt.Println("O diretório atual é:", diretorioAtual)
+}
+
+func copiarPasta(origem, destino string) error {
+	// Cria a pasta de destino
+	err := os.MkdirAll(destino, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	// Percorre os arquivos da pasta de origem
+	err = filepath.Walk(origem, func(caminhoOrigem string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Calcula o caminho de destino para o arquivo atual
+		caminhoDestino := filepath.Join(destino, caminhoOrigem[len(origem):])
+
+		if info.IsDir() {
+			// Cria a pasta no destino
+			err = os.MkdirAll(caminhoDestino, os.ModePerm)
+			if err != nil {
+				return err
+			}
+		} else {
+			// Copia o arquivo para o destino
+			arquivoOrigem, err := os.Open(caminhoOrigem)
+			if err != nil {
+				return err
+			}
+			defer arquivoOrigem.Close()
+
+			arquivoDestino, err := os.Create(caminhoDestino)
+			if err != nil {
+				return err
+			}
+			defer arquivoDestino.Close()
+
+			_, err = io.Copy(arquivoDestino, arquivoOrigem)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return err
 }
